8071a20cbcaa2c2d8acef4758a2d2b95604bf1a1
Nilesh Ghadge <nilesh.ghadge@inin.com>
2015-01-17 19:57:29 -0500

    Misc commits

---
+++ DesignPatterns/ObserverPattern/YoutubeChannel.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace ObserverPattern
+{
+    public class YoutubeChannel
+    {
+        private static YoutubeChannel _youtubeChannel;
+        private YoutubeChannel(){}
+
+        public static YoutubeChannel GetInstance()
+        {
+            return _youtubeChannel ?? (_youtubeChannel = new YoutubeChannel());
+        }
+
+        private readonly IList<ISubscriber> _listOfSubscribers = new List<ISubscriber>(); 
+        public void Subscribe(ISubscriber subscriber)
+        {
+            _listOfSubscribers.Add(subscriber);
+        }
+
+        public void UnSubscribe(ISubscriber subscriber)
+        {
+            _listOfSubscribers.Remove(subscriber);
+        }
+
+        public void Upload(string username)
+        {
+            foreach (var subscriber in _listOfSubscribers)
+            {
+                subscriber.Notify(username);
+            }
+        }
+    }
+}
